3f91a01d8b123a8e9d9af3b5de16a285551d5a9c
gideonchrapko <39398222+gideonchrapko@users.noreply.github.com>
2020-08-31 16:25:14 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three'
+import React, { Suspense, useState } from 'react'
+import { Canvas, Dom } from 'react-three-fiber'
+import Controls from './components/Controls'
+import Lights from './components/Lights'
+import Environment from './components/Environment'
+import Suzanne from './components/Suzanne'
+import Sphere from './components/Sphere'
+import Effects from './components/Effects'
+import './styles.css'
+
+export default function App() {
+  // Controls disable pointerevents on movement to save some CPU cost
+  const [active, set] = useState(false)
+  return (
+    <Canvas
+      concurrent
+      noEvents={active}
+      pixelRatio={window.devicePixelRatio}
+      camera={{ position: [0, 0, 6.5] }}
+      gl={{ antialias: false }}
+      onCreated={({ gl, scene }) => {
+        gl.toneMapping = THREE.ACESFilmicToneMapping
+        gl.outputEncoding = THREE.sRGBEncoding
+        scene.background = new THREE.Color('#373737')
+        //scene.background.convertSRGBToLinear()
+      }}>
+      <Lights />
+      <Controls disable={set} />
+      <Suspense fallback={<Dom center>loading...</Dom>}>
+        <Environment />
+        <Sphere position={[-2, 0, -1]} />
+        <Suzanne position={[2, 0, 1]} rotation={[0, -0.8, 0]} />
+        <Effects />
+      </Suspense>
+    </Canvas>
+  )
+}
---
+++ src/components/Controls.js
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react'
+import { extend, useThree, useFrame } from 'react-three-fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+extend({ OrbitControls })
+
+export default function Controls({ disable, ...props }) {
+  const { camera, gl } = useThree()
+  const ref = useRef()
+  useFrame(() => ref.current.update())
+
+  useEffect(() => {
+    if (disable) {
+      ref.current.addEventListener('start', () => disable(true))
+      ref.current.addEventListener('end', () => disable(false))
+    }
+  }, [])
+
+  return (
+    <orbitControls
+      ref={ref}
+      target={[0, 0, 0]}
+      enableDamping
+      enableZoom={false}
+      enablePan={false}
+      dampingFactor={0.05}
+      rotateSpeed={0.9}
+      minPolarAngle={Math.PI / 1.9}
+      maxPolarAngle={Math.PI / 1.9}
+      {...props}
+      args={[camera, gl.domElement]}
+    />
+  )
+}
